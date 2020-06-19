7d65d8a759c5562761e5ee34a9575f13586e9fcb
dosoung <soungdo0919@gmail.com>
2020-06-19 14:25:39 +0900

    separate

--- guide/lib/main.dart
+++ guide/lib/main.dart
@@ -18,12 +18,33 @@ class MyApp extends StatefulWidget {
 
 //<MyApp>사용 하면 MyApp에 속해 있는 클래스인것 을 안다.
 // _ 이 언더바는 private를 의미한다 다른곳에서 사용할 수 없게한다.
+
+final questions = [
+  {
+    'questionText': 'What\'s your favorite color?',
+    'answers': ['Blue', 'Black', 'White', 'Sky'],
+  },
+  {
+    'questionText': 'What\'s your favorite animal?',
+    'answers': ['Dog', 'Cat', 'Lion', 'Giraffe'],
+  },
+  {
+    'questionText': 'Who\'s your favorit instructor?',
+    'answers': ['Dohyun', 'SOUNG', 'DOSOUNG', 'Hello'],
+  },
+];
+
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   //build는 widget실행시 항상 필요함 widget을 리턴해야 하므로
   //widget은 항상 statelesswidget 또는 stateful widget을 가짐
 
   void _answerQuestion() {
+    var aBool = true;
+
+    if (_questionIndex < questions.length) {
+      print('We have more quiestions!');
+    }
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -31,21 +52,6 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Blue', 'Black', 'White', 'Sky'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Lion', 'Giraffe'],
-      },
-      {
-        'questionText': 'Who\'s your favorit instructor?',
-        'answers': ['Dohyun', 'SOUNG', 'DOSOUNG', 'Hello'],
-      },
-    ];
-
     //context 는 object이다.
     //MaterialApp and Text는 플러터에서 제공
     // <Widget>[] 이 리스트는 위젯의 타입을 갖는 리스트이다.
