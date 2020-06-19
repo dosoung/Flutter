53cc114350d2bf3ac30147a663bf4ce9d622d745
dosoung <soungdo0919@gmail.com>
2020-06-19 14:25:09 +0900

    separte widgets

--- realapp/lib/main.dart
+++ realapp/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './transaction.dart';
 import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
@@ -15,21 +14,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Grocereies',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
-
   // String titleInput;
   // String amountInput;
   final titleController = TextEditingController();
@@ -82,45 +66,7 @@ class MyHomePage extends StatelessWidget {
                 ),
               ),
             ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                    child: Row(
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue, width: 2)),
-                      child: Text(
-                        '\$\: ${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.blue),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ));
-              }).toList(),
-            )
+            Column()
           ],
         ));
   }
